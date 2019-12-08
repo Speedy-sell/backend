8e8d93c91548fe3c77d78efbd4a3fa4fc6c5919e
Nechar Joshi <joshinechar@gmail.com>
2019-12-08 15:17:03 +1100

    mongodb connection

---
+++ config/mongodb.config.ts
@@ -0,0 +1,8 @@
+const user = {
+  name: `Nechar`,
+  password: `6rrjC4wKBeUlAY6b`,
+};
+
+export const mongoDBConfig = {
+  key: `mongodb+srv://${user.name}:${user.password}@opensourcecluster-hv03t.mongodb.net/test?retryWrites=true&w=majority`,
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { mongoDBConfig } from '../config/mongodb.config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(mongoDBConfig.key, { useNewUrlParser: true }),
+  ],
   controllers: [CatsController],
   providers: [CatsService],
 })
-export class AppModule { }
+export class AppModule {}
