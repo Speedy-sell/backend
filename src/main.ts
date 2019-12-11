f3f2d711741d34967457a31d1dc72313249ecc54
Nechar Joshi <joshinechar@gmail.com>
2019-12-11 18:39:15 +1100

    Bootstrap change

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+const portNumber = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   setupSwagger(app);
-  await app.listen(3000);
+  await app.listen(portNumber);
+  console.info(`Application running on http://localhost:${portNumber}/`);
 }
 
 function setupSwagger(app) {
   const options = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description here')
+    .setTitle('Speedy Sell')
+    .setDescription('Buying and selling on steroids')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, options);
