96a3527f7e6ed8a77e9ebfb4462a962e8d0fa7ff
Nechar Joshi <Nechar.Joshi@resimac.com.au>
2019-12-18 09:59:02 +1100

    Console log before the app listens

--- config/mongodb.config.ts
+++ config/mongodb.config.ts
@@ -4,5 +4,5 @@ const collectionName = 'OpenSourceCollection';
 const extraParams = 'retryWrites=true&w=majority';
 
 export const mongoDBConfig = {
-  key: `mongodb+srv://Nechar:6rrjC4wKBeUlAY6b@opensourcecluster-hv03t.mongodb.net/test?retryWrites=true&w=majority`
+  key: `mongodb+srv://Nechar:6rrjC4wKBeUlAY6b@opensourcecluster-hv03t.mongodb.net/test?retryWrites=true&w=majority`,
 };
--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ const portNumber = 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
-  await app.listen(portNumber);
   console.info(`Application running on http://localhost:${portNumber}/`);
+  await app.listen(portNumber);
 }
 
 bootstrap();
