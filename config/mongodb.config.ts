d63aa499d71b44bb104932e87d5d5000a7865fff
Nechar Joshi <joshinechar@gmail.com>
2019-12-26 18:21:56 +1100

    Local Database Connect

--- config/mongodb.config.ts
+++ config/mongodb.config.ts
@@ -1,14 +1,22 @@
-const userName = `Nechar`;
-const password = `Rp9HiIAAIIf45wTF`;
-const collectionName = 'RootDB';
-const clusterName = 'cluster0-sumzs';
-const extraParams = 'retryWrites=true&w=majority';
-
 export const mongoDBConfig = {
-  key: `mongodb+srv://${userName}:${password}@${clusterName}.mongodb.net/${collectionName}?${extraParams}`,
+  key: getLocalKey(),
   options: {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
   },
 };
+
+function getRemoteKey() {
+  const userName = `Nechar`;
+  const password = `Rp9HiIAAIIf45wTF`;
+  const collectionName = 'RootDB';
+  const clusterName = 'cluster0-sumzs';
+  const extraParams = 'retryWrites=true&w=majority';
+  return `mongodb+srv://${userName}:${password}@${clusterName}.mongodb.net/${collectionName}?${extraParams}`;
+}
+
+function getLocalKey() {
+  const collectionName = 'RootDB';
+  return `mongodb://localhost:27017/${collectionName}`;
+}
