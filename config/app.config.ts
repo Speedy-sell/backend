11a42dd0d10abd109cad3c1cb38d13bfd1a4140e
nechar_joshi <joshinechar@gmail.com>
2019-12-30 14:15:17 +1100

    Resimac

--- config/app.config.ts
+++ config/app.config.ts
@@ -1,7 +1,7 @@
 export const config = {
   portNumber: 3000,
-  name: 'Speedy Sell',
-  description: 'Buying and selling on steroids',
-  version: 'Alpha',
+  name: 'Resimac Loan Api 1.0',
+  description: '/aresapi/swagger/v1/swagger.json',
+  version: 'v1',
   enableMockResponse: true,
 };
