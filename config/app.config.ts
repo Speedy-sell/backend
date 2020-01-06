afdf01e53b718db69efe5e590062c28101a7ceb6
nechar_joshi <joshinechar@gmail.com>
2020-01-06 13:53:03 +1100

    doc: some helpful console log

--- config/app.config.ts
+++ config/app.config.ts
@@ -1,9 +1,14 @@
+if (!process.env.SENDGRID_PASSWORD) {
+  // tslint:disable-next-line: no-console
+  console.warn(
+    `WARNING: Unable to find SENDGRID_PASSWORD. Your email might not work as expected`,
+  );
+}
 export const config = {
   portNumber: process.env.PORT || 3000,
   name: 'Resimac Loan Api 1.0',
   description: '/aresapi/swagger/v1/swagger.json',
   version: 'v1',
-  // TODO enable mock response only in dev environment
   enableMockResponse: process.env.ENABLE_MOCK_RESPONSE || false,
   hostURL: 'http://localhost:3000',
   emailProvider: {
@@ -11,6 +16,6 @@ export const config = {
     port: '587',
     secure: false, // true for 465, false for other ports
     username: 'apikey',
-    password: process.env.SEND_GRID_PASSWORD || 'undefined',
+    password: process.env.SENDGRID_PASSWORD || 'undefined',
   },
 };
