baa5ce0b33c35cefe55145e800bb7a6b6a34fcc8
Nechar Joshi <joshinechar@gmail.com>
2020-01-06 22:02:40 +1100

    Refactor: email service

--- src/services/email/email.service.ts
+++ src/services/email/email.service.ts
@@ -1,11 +1,22 @@
+// tslint:disable: no-console
+
 import { config } from '../../../config/app.config';
 import { Injectable } from '@nestjs/common';
 import { transporter } from '../../../config/email.config';
 
 @Injectable()
 export class EmailService {
-  async sendEmailVerification(emailToken): Promise<boolean> {
-    const mailOptions = {
+  private sendEmail(mailOptions) {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(`Unable to send: `, error);
+      }
+      console.log(`Message sent: %s`, info.messageId);
+    });
+  }
+
+  async sendEmailVerification(emailToken) {
+    this.sendEmail({
       from: `"No-Reply" <no-reply@yourDomain.com>`,
       to: `joshinechar@gmail.com`,
       subject: `Account Verification`,
@@ -13,21 +24,6 @@ export class EmailService {
         `Hi! <br><br> Please click ` +
         `<a href=${config.hostURL}/auth/verify/${emailToken}>this link</a>` +
         ` to activate your account. `,
-    };
-
-    const sent = await new Promise<boolean>(async (resolve, reject) => {
-      return await transporter.sendMail(mailOptions, async (error, info) => {
-        if (error) {
-          // tslint:disable-next-line: no-console
-          console.log(`Unable to send: `, error);
-          return reject(false);
-        }
-        // tslint:disable-next-line: no-console
-        console.log(`Message sent: %s`, info.messageId);
-        resolve(true);
-      });
     });
-
-    return sent;
   }
 }
