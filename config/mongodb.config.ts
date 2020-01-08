6d56b22606c08d679f40e2b90cb54d4450aa5de9
Nechar Joshi <joshinechar@gmail.com>
2020-01-09 07:39:03 +1100

    doc: move

--- config/mongodb.config.ts
+++ config/mongodb.config.ts
@@ -1,16 +1,16 @@
 export const mongoDBConfig = {
   key: getLocalKey(),
   options: {
+    /**
+     * Why are we using these `options`?
+     * Because if we didn't, the console keeps
+     * complaining that we didn't use these options.
+     * However, these options are optional
+     */
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
   },
-  /**
-   * Why are we using these `options`?
-   * Because if we didn't, the console keeps
-   * complaining that we didn't use these options.
-   * However, these options are optional
-   */
   collectionName: {
     user: 'User',
     item: 'Item',
