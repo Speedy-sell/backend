9cc1d8475e7b129f84c60940964463c3072a664b
Nechar <joshinechar@gmail.com>
2020-01-10 14:58:12 +1100

    Flip the logic

--- src/modules/users/auth/strategies/local.strategy.ts
+++ src/modules/users/auth/strategies/local.strategy.ts
@@ -14,9 +14,9 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
 
   async validate(email: string, password: string): Promise<any> {
     const user = await this.authService.validateUser(email, password);
-    if (!user) {
-      throw new UnauthorizedException();
+    if (user) {
+      return user;
     }
-    return user;
+    throw new UnauthorizedException();
   }
 }
